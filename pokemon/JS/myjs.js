61747371082f3d43e8e835587b001a95803a5a1f
Franco <fran.raggio@hotmail.com>
2020-04-26 16:00:32 -0300

    local storage completo

--- pokemon/JS/myjs.js
+++ pokemon/JS/myjs.js
@@ -28,6 +28,9 @@ function mostrarPokemon(pokemon){
     grp = document.getElementById('pokedex');
     var d1 = document.createElement('div');
     d1.className = "card";
+    var color = document.createElement('div');
+    color.className = "cardSub"
+    color.innerText=
     var img = document.createElement('img');
     img.className= "card-image";
     img.src=pokemon.sprites['front_default'];
@@ -45,6 +48,7 @@ function mostrarPokemon(pokemon){
     d1.appendChild(img);
     d1.appendChild(d2);
     grp.appendChild(d1);
+    d2.appendChild(color);
   }
 
 
